06d047d02d1ee0f8a47a240e01a0c8bc99b988ef
Ish Takkar <ishtakkar21@gmail.com>
2021-01-05 12:11:09 +0530

    added the footer component

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { Container, Inner, Item, Image, Pane, Title, SubTitle } from './styles/jumbotron';
+
+const Jumbotron = ({ children, direction = 'row', ...restProps }) => {
+    return (
+        <Item {...restProps}>
+            <Inner direction={direction}>
+                {children}
+            </Inner>
+        </Item>
+    );
+};
+
+Jumbotron.Container = function Jumbotron({ children, ...restProps }) {
+    return <Container {...restProps}>{children}</Container>
+}
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+    return <Pane {...restProps}>{children}</Pane>
+}
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
+    return <Title {...restProps}>{children}</Title>
+}
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+    return <SubTitle {...restProps}>{children}</SubTitle>
+}
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+    return <Image {...restProps} />
+}
+
+export default Jumbotron;
